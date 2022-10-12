e07df3377f710c10683eef29d915d2b549474aee
Nic Durish <n.durish@gmail.com>
2022-10-12 14:02:06 -0400

    hotfix netlify building tailwind

---
+++ src/_data/site.js
@@ -0,0 +1,10 @@
+// Located in _data/meta.js
+module.exports = {
+    // NOTE: `process.env.URL` is provided by Netlify, and may need
+    // adjusted pending your host
+    url: 'http://localhost:8080',
+    title: 'Veranda Digital Inc',
+    description: 'A starter template to build websites with Ghost & Eleventy TESTTESTTEST',
+    authorName: 'Nic Durish',
+    twitter: '' // no `@`
+};
