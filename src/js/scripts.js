44b8157ca58749d2ae3fb3fbdd457a6deb479eb1
Nic Durish <n.durish@gmail.com>
2022-10-16 13:00:19 -0400

    readding light font

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,5 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+// import AOS from 'aos';
+// import 'aos/dist/aos.css';
 
 // var playButton = document.getElementById('play_button');
 // var video = document.getElementById('commercial');
