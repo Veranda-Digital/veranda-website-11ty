c0ef6b0a962955b545ca31f02dca7443d7e58f5b
Nic Durish <n.durish@gmail.com>
2024-12-20 09:50:53 -0500

    adding styles to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,12 +35,14 @@ module.exports = {
             xsm: '0.7rem',
             sm: '0.8rem',
             base: '1rem',
+            press: ['1.15rem', '1.6'],
             med: '1.25rem',
             lg: ['1.5rem', '34px'],
             lgr: '2rem',
             xlg: ['2.2rem', '45px'],
             xxlg: ['2.8rem', '56px'],
             xxxlg: ['4rem', '75px'],
+            xxxxlg: ['6.5rem', '95px'],
             huge: ['9.5rem', '120px'],
             xhuge: ['12rem', '10rem']
         },
@@ -59,6 +61,9 @@ module.exports = {
             },
             borderRadius: {
                 '4xl': '2rem'
+            },
+            width: {
+                '80vw': '80vw' // Custom width class
             }
         }
     }
